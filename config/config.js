5f9985e849b9f7973361ac18a472b833f91c9b70
Johann <baptistajohann@gmail.com>
2017-12-04 13:43:00 -0800

    stuff

--- config/config.js
+++ config/config.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
+
 {
   "development": {
-    "username": "AustinJohann",
-    "password": "password",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
     "database": "assignment_calendar_event_planner_development",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "AustinJohann",
-    "password": "password",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
     "database": "assignment_calendar_event_planner_test",
     "host": "127.0.0.1",
     "dialect": "postgres"
