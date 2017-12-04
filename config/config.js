de8e138e3736ad5b8a5ee340ee00867682a0b8fa
Johann <baptistajohann@gmail.com>
2017-12-04 14:21:14 -0800

    REPL and seeds workinggit add -Agit add -A!

--- config/config.js
+++ config/config.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
+require('dotenv').config({path: '/home/alan/Documents/SoftwareDevelopment/VikingCodeSchool/coursework/projects/assignment_calendar_event_planner/config/.env'});
 
-{
-  "development": {
-    "username": process.env.POSTGRES_USERNAME,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": "assignment_calendar_event_planner_development",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+console.log(process.env.POSTGRES_USERNAME);
+console.log(process.env.POSTGRES_PASSWORD);
+
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: 'assignment_calendar_event_planner_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
   },
-  "test": {
-    "username": process.env.POSTGRES_USERNAME,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": "assignment_calendar_event_planner_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: 'assignment_calendar_event_planner_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
   },
-  "production": {
-    "username": "AustinJohann",
-    "password": "password",
-    "database": "assignment_calendar_event_planner_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  }
-}
+  production: {
+    username: 'AustinJohann',
+    password: 'password',
+    database: 'assignment_calendar_event_planner_production',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
