0c0c97ffb6d5ac7de8d67715948a7d46c555f7e8
Austin1780 <austinwilliamsmith@gmail.com>
2017-12-04 16:09:33 -0700

    finished index page with data

--- config/config.js
+++ config/config.js
@@ -1,5 +1,9 @@
-require('dotenv').config({path: '/home/alan/Documents/SoftwareDevelopment/VikingCodeSchool/coursework/projects/assignment_calendar_event_planner/config/.env'});
+var path = require("path");
+require("dotenv").config({
+  path: path.join(__dirname, "/.env")
+});
 
+console.log(path.join(__dirname, "/.env"));
 console.log(process.env.POSTGRES_USERNAME);
 console.log(process.env.POSTGRES_PASSWORD);
 
@@ -7,22 +11,22 @@ module.exports = {
   development: {
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
-    database: 'assignment_calendar_event_planner_development',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    database: "assignment_calendar_event_planner_development",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
   test: {
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
-    database: 'assignment_calendar_event_planner_test',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    database: "assignment_calendar_event_planner_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
   production: {
-    username: 'AustinJohann',
-    password: 'password',
-    database: 'assignment_calendar_event_planner_production',
-    host: '127.0.0.1',
-    dialect: 'postgres',
-  },
+    username: "AustinJohann",
+    password: "password",
+    database: "assignment_calendar_event_planner_production",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
+let models = require("./../models");
+let User = models.User;
+let sequelize = models.sequelize;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  User.findAll().then(users => {
+    res.render("index", { title: "Calendar", users: users });
+  });
 });
 
 module.exports = router;
